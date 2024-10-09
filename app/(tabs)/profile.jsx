c22aec507a36dfe78cd781cf325d04776fa9eece
Krisnaaassss <krisnalucianosiburian29@students.unnes.ac.id>
2024-10-09 14:52:45 +0700

    make profile page

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import UserInfo from "../../components/Profile/UserInfo";
+import MenuList from "../../components/Profile/MenuList";
 
 export default function profile() {
   return (
-    <View>
-      <Text>profile</Text>
+    <View style={{ padding: 20 }}>
+      <Text style={{ fontFamily: "outfit-bold", fontSize: 30, paddingTop: 20 }}>
+        Profile
+      </Text>
+
+      {/* User Intro */}
+      <UserInfo />
+      {/* Menu List */}
+      <MenuList />
     </View>
-  )
+  );
 }
---
+++ components/Profile/UserInfo.jsx
@@ -0,0 +1,29 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { useUser } from "@clerk/clerk-expo";
+
+export default function UserInfo() {
+  const { user } = useUser();
+
+  return (
+    <View
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 20,
+      }}
+    >
+      <Image
+        source={{ uri: user.imageUrl }}
+        style={{ width: 100, height: 100, borderRadius: 99 }}
+      />
+      <Text style={{ fontFamily: "outfit-bold", fontSize: 20 }}>
+        {user?.fullName}
+      </Text>
+      <Text style={{ fontFamily: "outfit", fontSize: 15 }}>
+        {user?.primaryEmailAddress.emailAddress}
+      </Text>
+    </View>
+  );
+}
